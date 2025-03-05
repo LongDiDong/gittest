321db0bcfbbe866eb2f1d169780739413bfcde27
RenSiming <1016501602@qq.com>
2025-03-05 13:38:08 +0800

    中文

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
  * @Author: RenSiming 1016501602@qq.com
  * @Date: 2025-03-05 11:55:46
  * @LastEditors: RenSiming 1016501602@qq.com
- * @LastEditTime: 2025-03-05 13:35:03
+ * @LastEditTime: 2025-03-05 13:38:02
  * @FilePath: \gittest\test.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
