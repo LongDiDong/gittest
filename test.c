26ee325ca07ef6189bea52018553231b20b5764d
RenSiming <1016501602@qq.com>
2025-03-05 12:24:19 +0800

    2

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
  * @Author: RenSiming 1016501602@qq.com
  * @Date: 2025-03-05 11:55:46
  * @LastEditors: RenSiming 1016501602@qq.com
- * @LastEditTime: 2025-03-05 11:56:22
+ * @LastEditTime: 2025-03-05 12:24:12
  * @FilePath: \gittest\test.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,4 +11,6 @@
 int main() {
     printf("Hello, World!\n");
     return 0;
+
+    
 }
